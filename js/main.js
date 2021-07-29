b7ad9458e1d2acd9f450e541b99ed3398663419d
sadatt254 <sadatt254@gmail.com>
2021-07-29 19:48:11 +0300

    new landing page

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
-const countrySelect = document.querySelector('#country-select');
-const countryFlag = document.querySelector('.country-flag');
+// const countrySelect = document.querySelector('#country-select');
+// const countryFlag = document.querySelector('.country-flag');
+
 
 fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(
         data => {
-            countryFlag.innerHTML = ""
-            for (let i = 0; i < data.length; i++){
-                countryFlag.innerHTML = `<img src=${data[i].flag} alt='${data[0].name}-flag'>`
-                countrySelect.innerHTML += `<option value="">${data[i].name}</option>`
-            }
+            const americas = data.filter(item => {
+                return item.region;
+            })
+
+            console.log(americas)
+            // const africa = data.filter(item => {
+            //     return item.region == "Africa"
+            // })
+
+            // const europe = data.filter(item => {
+            //     return item.region == "Europe"
+            // })
+
+            // const asia = data.filter(item => {
+            //     return item.region == "Asia"
+            // })
+            // for (let i = 0; i < data.length; i++){
+            //     console.log(data[i])
+            //     countryFlag.innerHTML = `<p>You selected</p> <img src=${data[i].flag} alt='${data[0].name}-flag'> <p>${data[i].name}</p>`
+            //     countrySelect.innerHTML += `<option value="" onclick='flagDisplay(${i})'>${data[i].name}</option>`
+            // }
         }
     )
     .catch(err => console.log(err))
