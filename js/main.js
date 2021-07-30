73990d390be054d55b0c898e99c2d65556c88122
sadatt254 <sadatt254@gmail.com>
2021-07-30 16:56:17 +0300

    countries added

--- js/main.js
+++ js/main.js
@@ -11,31 +11,50 @@ window.addEventListener('scroll', e => {
     }
 })
 
+const eu = document.querySelector('.europe');
+const amer = document.querySelector('.americas');
+const afr = document.querySelector('.africa');
+const asia = document.querySelector('.asia');
+
+eu.innerHTML = ""
+amer.innerHTML = ""
+afr.innerHTML = ""
+asia.innerHTML = ""
+
 fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
     .then(
         data => {
-            const americas = data.filter(item => {
-                return item.region;
+            const europe = data.filter(dt => {
+                return dt.region == "Europe";
+            })
+            for (let i in europe){
+                eu.innerHTML += `<li><img src="${europe[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="../pages/services.html">${europe[i].name}</a></li>`
+            }
+
+            const americas = data.filter(dt => {
+                return dt.region == "Americas";
             })
 
-            // console.log(americas)
-            // const africa = data.filter(item => {
-            //     return item.region == "Africa"
-            // })
+            for (let i in americas){
+                amer.innerHTML += `<li><img src="${americas[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="../pages/services.html">${americas[i].name}</a></li>`
+            }
 
-            // const europe = data.filter(item => {
-            //     return item.region == "Europe"
-            // })
+            const africa = data.filter(dt => {
+                return dt.region == "Africa";
+            })
 
-            // const asia = data.filter(item => {
-            //     return item.region == "Asia"
-            // })
-            // for (let i = 0; i < data.length; i++){
-            //     console.log(data[i])
-            //     countryFlag.innerHTML = `<p>You selected</p> <img src=${data[i].flag} alt='${data[0].name}-flag'> <p>${data[i].name}</p>`
-            //     countrySelect.innerHTML += `<option value="" onclick='flagDisplay(${i})'>${data[i].name}</option>`
-            // }
+            for (let i in africa){
+                afr.innerHTML += `<li><img src="${africa[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="../pages/services.html">${africa[i].name}</a></li>`
+            }
+
+            const asia = data.filter(dt => {
+                return dt.region == "Asia";
+            })
+
+            for (let i in asia){
+                asia.innerHTML += `<li><img src="${asia[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="../pages/services.html">${asia[i].name}</a></li>`
+            }
         }
     )
     .catch(err => console.log(err))
