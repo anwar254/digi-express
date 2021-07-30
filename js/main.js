8f2ef45cbe347888dd951a2a1aa621573ceece17
sadatt254 <sadatt254@gmail.com>
2021-07-30 16:46:14 +0300

    added the countries

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const eu = document.querySelector('.europe');
 const amer = document.querySelector('.americas');
+const afr = document.querySelector('.africa');
+const asia = document.querySelector('.asia');
 
 eu.innerHTML = ""
 amer.innerHTML = ""
+afr.innerHTML = ""
+asia.innerHTML = ""
 
 fetch('https://restcountries.eu/rest/v2/all')
     .then(res => res.json())
@@ -12,8 +16,31 @@ fetch('https://restcountries.eu/rest/v2/all')
                 return dt.region == "Europe";
             })
             for (let i in europe){
-                console.log(europe)
-                eu.innerHTML = `<li><img src="${europe[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="">${europe[i].name}</a></li>`
+                eu.innerHTML += `<li><img src="${europe[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="">${europe[i].name}</a></li>`
+            }
+
+            const americas = data.filter(dt => {
+                return dt.region == "Americas";
+            })
+
+            for (let i in americas){
+                amer.innerHTML += `<li><img src="${americas[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="">${americas[i].name}</a></li>`
+            }
+
+            const africa = data.filter(dt => {
+                return dt.region == "Africa";
+            })
+
+            for (let i in africa){
+                afr.innerHTML += `<li><img src="${africa[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="">${africa[i].name}</a></li>`
+            }
+
+            const asia = data.filter(dt => {
+                return dt.region == "Asia";
+            })
+
+            for (let i in asia){
+                asia.innerHTML += `<li><img src="${asia[i].flag}" alt="Argentina" width="16" height="11">&nbsp;<a href="">${asia[i].name}</a></li>`
             }
         }
     )
